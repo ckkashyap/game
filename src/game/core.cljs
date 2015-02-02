83dac252a2f897b07c6487d192458aac22cfae38
CK Kashyap <ckk@microsoft.com>
2015-02-02 06:03:12 -0500

    added another file

--- src/game/core.cljs
+++ src/game/core.cljs
@@ -1,22 +1,25 @@
 (ns game.core
-  (:require [clojure.browser.repl :as repl]))
+;  (:require [clojure.browser.repl :as repl])
+  (:require [game.createjs :as createjs])
+)
 
 ;; (repl/connect "http://localhost:9000/repl")
 
 (enable-console-print!)
 
+(def stage)
 
 (defn ^:export init []
   (let [
-        s (new  js/createjs.Stage "demoCanvas")
+        _ (set! stage (new  js/createjs.Stage "demoCanvas"))
         c (new js/createjs.Shape)
         a1 (.drawCircle (.beginFill (.-graphics c) "Blue") 0 5 50)
         a2 (set! (.-x c) 100)
         a3 (set! (.-y c) 100)
-        a4 (.addChild s c)
-        a5 (.update s)
+        a4 (.addChild stage c)
+        a5 (.update stage)
         ]
-  (js/alert "AAA")))
+  (js/alert (createjs/dingo))))
 
 
 
---
+++ src/game/createjs.cljs
@@ -0,0 +1,5 @@
+(ns game.createjs)
+
+
+(defn dingo []
+  (js/alert "in another file"))
