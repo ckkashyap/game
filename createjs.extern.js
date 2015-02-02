83d574bf8c79b38869e390e42dfe0343950e996b
CK Kashyap <ckk@microsoft.com>
2015-02-02 03:03:11 -0500

    the debug version works

---
+++ createjs.extern.js
@@ -0,0 +1,14 @@
+var createjs = {
+    var Stage = function() {}
+    var Shape= function () {
+	var graphics =function() {
+	    var beginFill = function (a) {
+		var drawCircle = function (a,b,c,d) {}
+	    }
+	}
+    };
+}
+
+var x = createjs.Shape().graphics.beginFill("A").drawCircle(1,2,3,4)
+
+var beginFill={}
---
+++ src/game/core.cljs
@@ -0,0 +1,25 @@
+(ns game.core
+  (:require [clojure.browser.repl :as repl]))
+
+;; (repl/connect "http://localhost:9000/repl")
+
+(enable-console-print!)
+
+
+(defn ^:export init []
+  (let [
+        s (new  js/createjs.Stage "demoCanvas")
+        c (new js/createjs.Shape)
+        a1 (.drawCircle (.beginFill (.. c -graphics) "Red") 0 5 50)
+        a2 (set! (.-x c) 100)
+        a3 (set! (.-y c) 100)
+        a4 (.addChild s c)
+        a5 (.update s)
+        ]
+  (js/alert "AAA")))
+
+
+
+(println "Hello world!")
+
+
